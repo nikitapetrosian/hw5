8353aaa0245c18dc2fcea4367dd761ca3d4f5bca
Nikita <nekitrus92@yandex.ru>
2021-09-12 17:02:57 +0300

    finaly

---
+++ src/components/groupList.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import PropTypes from "prop-types";
+const GroupList = ({
+    items,
+    valueProperty,
+    contentProperty,
+    onItemsSelect,
+    selectedItems
+}) => {
+    return (
+        <ul className="list-group">
+            {Object.keys(items).map((item) => (
+                <li
+                    key={items[item][valueProperty]}
+                    className={
+                        "list-group-item" +
+                        (selectedItems.includes(items[item][contentProperty]) ? " active" : "")
+                    }
+                    onClick={() => onItemsSelect(items[item][contentProperty])}
+                    role="button"
+                >
+                    <input className="form-check-input" type="checkbox" value={items[item][valueProperty]}
+                        checked={selectedItems.includes(items[item][contentProperty])}
+                        onChange={() => { }}
+                    />
+
+                    {items[item][contentProperty]}
+                </li>
+            ))}
+        </ul>
+    );
+};
+GroupList.defaultProps = {
+    contentProperty: "name",
+    valueProperty: "_id"
+};
+GroupList.propTypes = {
+    items: PropTypes.object.isRequired,
+    contentProperty: PropTypes.string.isRequired,
+    valueProperty: PropTypes.string.isRequired,
+    onItemsSelect: PropTypes.func,
+    selectedItems: PropTypes.array
+};
+export default GroupList;
--- src/components/users.jsx
+++ src/components/users.jsx
@@ -1,8 +1,9 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import api from "../api";
 import { FavoriteBtn } from "./favoriteBtn";
 import Pagination from "./pagination";
 import { paginate } from "../utils/paginate";
+import GroupList from "./groupList";
 const Users = () => {
     const [users, setUsers] = useState(api.users.fetchAll());
     const onDelete = (id) => {
@@ -10,73 +11,117 @@ const Users = () => {
         setUsers(newArr);
     };
     const [currentPage, setCurrentPage] = useState(1);
+    const [professions, setProfessions] = useState();
+    const [selectedProf, setSelectedProf] = useState([]);
     const count = users.length;
     const pageSize = 4;
-    const handlePageCgange = (pageIndex) => {
+    useEffect(() => {
+        api.professions.fetchAll().then((data) => setProfessions(data));
+    }, []);
+
+    const handleProffesionSelect = (item) => {
+        if (selectedProf.includes(item)) {
+            const indexItem = selectedProf.indexOf(item);
+            const newArr = [...selectedProf];
+            newArr.splice(indexItem, 1);
+            console.log(newArr);
+            setSelectedProf(newArr);
+        } else {
+            setSelectedProf([...selectedProf, item]);
+            setCurrentPage(1);
+        }
+    };
+    const handlePageChange = (pageIndex) => {
         // console.log(pageIndex);
         setCurrentPage(pageIndex);
     };
-    const userCrop = paginate(users, currentPage, pageSize);
+    const clearFilter = () => {
+        setSelectedProf([]);
+    };
+    const filteredUsers = selectedProf.length ? users.filter((user) => selectedProf.includes(user.profession.name)) : users;
+    const userCrop = paginate(filteredUsers, currentPage, pageSize);
     return (
-        <>
-            <h1>
-                <span className="badge bg-secondary">
-                    {users?.length ? `с тобой тусует ${count}` : "никого нет"}
-                </span>
-            </h1>
-            <table className="table">
-                <thead>
-                    <tr>
-                        <th scope="col">Имя</th>
-                        <th scope="col">Качества</th>
-                        <th scope="col">Профессия</th>
-                        <th scope="col">Встретился, раз</th>
-                        <th scope="col">Оценка</th>
-                        <th csope="col">Избранное</th>
-                        <th></th>
-                    </tr>
-                </thead>
+        <div className="d-flex">
+            {professions && (
+                <div className="d-flex flex-column flex-shrink-0 p-3">
 
-                <tbody>
-                    {userCrop.map((user) => (
-                        <tr key={user._id}>
-                            <td>{user.name}</td>
-                            <td>
-                                {user.qualities.map((quality) => (
-                                    <span
-                                        className={`p-1 m-2 bg-${quality.color} text-white smallText `}
-                                        key={quality._id}
-                                    >
-                                        {quality.name}
-                                    </span>
-                                ))}
-                            </td>
-                            <td>{user.profession.name}</td>
-                            <td>{user.completedMeetings}</td>
-                            <td>{user.rate}</td>
-                            <td>
-                                <FavoriteBtn />
-                            </td>
-                            <td>
-                                <button
-                                    className="badge bg-danger"
-                                    onClick={() => onDelete(user._id)}
-                                >
-                                    delete
-                                </button>
-                            </td>
+                    <GroupList
+                        selectedItems={selectedProf}
+                        items={professions}
+                        onItemsSelect={handleProffesionSelect}
+                    />
+                    <button className='btn btn-secondary mt-2'
+                        onClick={clearFilter}
+                    >
+                        Очистить фильр
+                    </button>
+                </div>
+            )}
+
+            <div className="d-flex flex-column">
+                <h1>
+                    <span className="badge bg-secondary">
+                        {users?.length ? `с тобой тусует ${filteredUsers.length}` : "никого нет"}
+                    </span>
+                </h1>
+                <table className="table">
+
+                    <thead>
+                        <tr>
+                            <th scope="col">Имя</th>
+                            <th scope="col">Качества</th>
+                            <th scope="col">Профессия</th>
+                            <th scope="col">Встретился, раз</th>
+                            <th scope="col">Оценка</th>
+                            <th csope="col">Избранное</th>
+                            <th></th>
                         </tr>
-                    ))}
-                </tbody>
-            </table>
+                    </thead>
 
-            <Pagination
-                itemsCount={count}
-                pageSize={pageSize}
-                currentPage={currentPage}
-                onPageChange={handlePageCgange}
-            />
-        </>
+                    <tbody>
+                        {userCrop.map((user) => (
+                            <tr key={user._id}>
+                                <td>{user.name}</td>
+                                <td>
+                                    {user.qualities.map((quality) => (
+                                        <span
+                                            className={`p-1 m-2 bg-${quality.color} text-white smallText `}
+                                            key={quality._id}
+                                        >
+                                            {quality.name}
+                                        </span>
+                                    ))}
+                                </td>
+                                <td>{user.profession.name}</td>
+                                <td>{user.completedMeetings}</td>
+                                <td>{user.rate}</td>
+                                <td>
+                                    <FavoriteBtn />
+                                </td>
+                                <td>
+                                    <button
+                                        className="badge bg-danger"
+                                        onClick={() => onDelete(user._id)}
+                                    >
+                                        delete
+                                    </button>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+                <div className="d-flex justify-content-center">
+                    <Pagination
+                        itemsCount={count}
+                        pageSize={pageSize}
+                        currentPage={currentPage}
+                        onPageChange={handlePageChange}
+                    />
+                </div>
+            </div>
+
+        </div>
+
     );
 };
 
